25c4fd2fd32322035415db5d01ab07a85af08024
javierd4mir <javier.damir@gmail.com>
2026-03-03 23:03:31 -0600

    Create script.js
    Sistema está funcionando.

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+Archivo de funcionalidad de la plataforma
+Aquí se escribirá la lógica del sistema utilizando JavaScript
+*/
+
+/* 
+La función se ejecuta cuando la página termina de cargarse.
+Esto asegura que todos los elementos HTML ya estén disponibles.
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Mensaje en la consola del navegador para confirmar que el sistema funciona
+    console.log("La plataforma de perfil profesional se ha cargado correctamente.");
+
+});
